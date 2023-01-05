fd6f063060d0e76ffe053c521e70f82ae81f20b1
KaitoMuraoka <kaitoboshi@icloud.com>
2023-01-05 22:07:08 +0900

    U_s, U_w演算子を作成

--- 研究/operation.py
+++ 研究/operation.py
@@ -1,6 +1,21 @@
 ## ライブラリのインポート
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import numpy as np
+import time
+import random
+from qulacs import QuantumState
+from qulacs.state import inner_product
 from qulacs import QuantumCircuit
+from qulacs.gate import to_matrix_gate
+from qulacs import QuantumState
+from qulacs.gate import Identity, X,Y,Z #パウリ演算子
 from qulacs.gate import H
+from qulacs.gate import RX,RY,RZ #パウリ演算子についての回転演算
+from qulacs.gate import U1,U2,U3 #IBMQの基底ゲート
+
+from fractions import Fraction
+
 
 # アダマール演算子
 def Hadamard(nqubits):
@@ -8,3 +23,39 @@ def Hadamard(nqubits):
     for i in range(nqubits):
         Hadamard.add_gate(H(i))
     return Hadamard
+
+# オラクルU_wの作成
+def make_U_w(nqubits):
+    U_w = QuantumCircuit(nqubits)
+    CnZ = to_matrix_gate(Z(nqubits-1))
+    # i-th qubitが全て1の場合だけゲートを作用
+    for i in range(nqubits-1):
+        control_index = i
+        control_with_value = 1
+        CnZ.add_control_qubit(control_index, control_with_value)
+    U_w.add_gate(CnZ)
+    return U_w
+
+# 反転U_sを作成
+# 反転U_sを作る
+def make_U_s(nqubits):
+    U_s = QuantumCircuit(nqubits)
+    for i in range(nqubits):
+        U_s.add_gate(H(i))
+
+    ## 2|0><0| - I の実装
+    U_s.add_gate(to_matrix_gate(RZ(nqubits-1, 2*np.pi))) ## まず、位相(-1)を全ての状態に付与する。ゲート行列はarrary([[-1,0],[0,-1]])
+    U_s.add_gate( X(nqubits-1) )
+    ## 全てのi-th qubitが0の場合だけZゲートを作用させる
+    CnZ = to_matrix_gate(Z(nqubits-1))
+    for i in range(nqubits-1):
+        control_index = i
+        control_with_value = 0
+        CnZ.add_control_qubit(control_index, control_with_value)
+    U_s.add_gate( CnZ )
+    U_s.add_gate( X(nqubits-1) )
+
+    for i in range(nqubits):
+        U_s.add_gate(H(i))
+
+    return U_s
