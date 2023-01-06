85adafaf34630c7bc398295ba7cf4977aa6003e0
KaitoMuraoka <kaitoboshi@icloud.com>
2023-01-06 14:34:08 +0900

    グラフをリファクタリング

--- 研究/made_graph.py
+++ 研究/made_graph.py
@@ -34,3 +34,40 @@ def line_graph(result, title):
     plt.xticks(np.arange(0, len(result), step=1))
     plt.grid()
     plt.show()
+
+## ここからノイズ本題
+noises_title = ['0', '(π/4)',  'π/2', '3π/4', 'π', '5π/4', '3π/2',  '7π/4',  '2π']
+noises_title_int = [0, (np.pi/4),  np.pi/2, 3*np.pi/4, np.pi, 5*np.pi/4, 3*np.pi/2,  7*np.pi/4,  2*np.pi]
+
+# K - K_throryのグラフ化
+def different_of_k_graph(x, y, yticks_start, yticks_end):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xticks(noises_title_int, noises_title)
+    ax.set_yticks(np.arange(yticks_start, yticks_end, 1))
+    ax.grid(axis = 'both')
+    ax.set_xlabel("与えたノイズδ", size = 14, weight = "light")
+    ax.set_ylabel('(k) - (k_theory)', size = 14, weight = "light")
+    ax.plot(x, y, "-o")
+
+# P(k)のグラフ化
+def P_k_graph(x, y):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xticks(noises_title_int, noises_title)
+    ax.set_ylim(0, 1)
+    ax.grid(axis = 'both')
+    ax.set_xlabel("与えたノイズδ", size = 14, weight = "light")
+    ax.set_ylabel("解の確率振幅", size = 14, weight = "light")
+    ax.plot(x, y, "-o")
+
+# P(K_throry)のグラフ化
+def P_k_throry_graph(x, y):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xticks(noises_title_int, noises_title)
+    ax.set_ylim(0, 1)
+    ax.grid(axis = 'both')
+    ax.set_xlabel("与えたノイズδ", size = 14, weight = "light")
+    ax.set_ylabel("理想値k_{theory}時の解の確率振幅", size = 14, weight = "light")
+    ax.plot(x, y, "-o")
